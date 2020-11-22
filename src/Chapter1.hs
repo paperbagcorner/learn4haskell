af87f74b4f8af937116f1513f8f0074c3d02bbea
Mattias Andersson <mandersson444@gmail.com>
2020-11-22 12:38:57 +0100

    Task 7

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -556,7 +556,12 @@ value after "=" where the condition is true.
 Casual reminder about adding top-level type signatures for all functions :)
 -}
 
-mid x y z = error "mid: not implemented!"
+mid :: Int -> Int -> Int -> Int
+mid x y z
+  | x < y && x > z = x
+  | y < z && y > x = y
+  | otherwise = z
+
 
 {- |
 =⚔️= Task 8
