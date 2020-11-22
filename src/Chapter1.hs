b247c64f563c829c7cba0970b44cd07cd0c194ed
Mattias Andersson <mandersson444@gmail.com>
2020-11-22 12:18:48 +0100

    Task 5

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -490,7 +490,8 @@ Implement a function that returns the last digit of a given number.
   whether it works for you!
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Int -> Int
+lastDigit n = mod (abs n) 10
 
 
 {- |
